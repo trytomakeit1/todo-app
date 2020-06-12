9c778e8524893f66b8438ee8de813351e4e49472
trytomakeit1 <trytomakeit1@gmail.com>
2020-06-12 11:46:01 +0430

    setting up the environment and config files

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public' ,
+        filename: 'bundle.js'
+
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{     
+                        cacheDirectory: true
+                    }
+                }        
+            },
+
+            {
+                test: '/\.css$/',
+                use:{
+                    loader: 'css-loader'
+                }
+            }
+            
+        ]
+    },
+
+    devServer: {
+        port: 3000,
+        contentBase: './public',
+        inline: true /* automatically updates live code */
+    }
+}
