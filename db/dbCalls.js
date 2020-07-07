6f9e757f96db88fcc5c5f1d25ac73a8282763205
trytomakeit1 <trytomakeit1@gmail.com>
2020-07-07 13:13:34 +0430

    DB connection error control

--- db/dbCalls.js
+++ db/dbCalls.js
@@ -8,13 +8,13 @@ const collection = 'tasks';
 const tasksToInsert = [{
     "title": "first note",
     "description": "this is my first task to do",
-    "date": 1591961611970,
+    "date": "12/05/2018",
     "finished": true
     },
     {
     "title": "second note",
     "description": "this is my second task to do",
-    "date": 1591961813171,
+    "date": "31/12/2019",
     "finished": false
 }];
 
@@ -22,23 +22,23 @@ const insertTasksToDB = () => {
 
     mongoClient.connect(url, (err, client) => {
 
-        let db = client.db(dbName);
-        //insert data to tasks collection
-        if(err) {
-            console.log("there was an error in connecting to DB");
+        if(err) 
+            console.error("Error-dbCalls-insertTasksToDB: problem connecting to DB while adding tasks. error:", err); 
 
-        } else {
+        else {
+            let db = client.db(dbName);
+            //insert data to tasks collection
+
             db.collection(collection).insertMany(tasksToInsert,
                 (err, result) => {
                     if(err) {
-                        console.log(`there was an error in inserting
-                         the tasks to DB`);
+                        console.error("Error-dbCalls-findAllTasks: problem finding tasks list. error:", err); 
+                        //TODO
                         client.close();
                     }
-                    console.log("tasks inserted successfully", result);
+                    console.log("Tasks inserted successfully", result);
 
             });
-
         }
     })
 }
@@ -49,12 +49,17 @@ const findAllTasks = (cb) => {
 
     mongoClient.connect(url, (err, client) =>{
 
+        if(err) {console.error("Error-dbCalls-findAllTasks: problem connecting to DB while fetching tasks. error:", err); 
+            cb("*Error: an error occured while fetching the tasks.");
+
+        }
+
         let databaseName = client.db(dbName);
         databaseName.collection(collection).find({}).toArray(function(err,result) {
-            if(err) {console.log(err); cb(err);}
+            if(err) {console.error("Error-dbCalls-findAllTasks: problem finding tasks list. error:", err); 
+                cb("*Error: an error occured while fetching the tasks.");
 
-            else {
-
+            } else {
                 cb(null, result);
 
             }
@@ -65,15 +70,25 @@ const findAllTasks = (cb) => {
 
 const findTask = (taskId, cb) => {
 
+    if(!taskId) {
+        console.log("Error-dbCalls-findTask: missing taskId:", taskId);
+        cb("*Error: can not fetch the details.");
+    
+    }
     // typeof taskId ObjectId
     mongoClient.connect(url, (err, client) => {
+
+        if(err) {console.error("Error-dbCalls-findTask: problem connecting to DB while fetching details of taskId:", taskId, " error:", err); 
+            cb("*Error: an error occured while fetching the details");
+        }
         
         let db = client.db(dbName);
         db.collection(collection).findOne({_id: new ObjectID(taskId)}, (err, result)=>{
             
-            if(err) {console.log(err); cb(err);}
-            
-            else {
+            if(err) {console.error("Error-dbCalls-findTask: problem finding details of taskId:", taskId, " error:", err); 
+                cb("*Error: an error occured while fetching the details");
+
+            } else {
                 cb(null, result);
             }
         })
@@ -85,31 +100,30 @@ const findTask = (taskId, cb) => {
 
 const addTask = (task, cb) =>{
 
-    //check if all fields are filled correctly (FE and BE)
-
     if(!task.title || !task.description || !task.date) {
-        console.log("some fields missing");
+
+        console.log("Error-dbCalls-addTask: missing fields in inserting task:", task);
+        cb("*Error: can not add the new task. some fields are missing.");
     }
     if(!task.finished) {
         task.finished = false;
     }
-    //let datePieces = task.date.split("-");
-    // supported date formats in Javascript: YYYY-MM-DD or MM/DD/YYYY or 25 Mar 2015 or Mar 25 2015
-    
-    //let newDate = datePieces[2]  + "-" + datePieces[1] + "-" + datePieces[0];
-    //task.date = Number(new Date(newDate));
 
-    console.log("task to be added:", task);
     mongoClient.connect(url, (err, client) => {
 
+        if(err) {
+            console.error("Error-dbCalls-addTask: problem connecting to DB while inserting task:", task, " error:", err);
+            cb("*Error: an error occured while adding the task");
+        }
         let db = client.db(dbName);
         db.collection(collection).insertOne(task, (err, result) => {
 
             if(err) {
-                console.log("Error in inserting task", task);
-                cb(err);}
-            else {
-                cb(null, "Successfully added the task");}
+                console.error("Error-dbCalls-addTask: problem inserting task:", task, " error:", err);
+                cb("*Error: an error occured while adding the task");
+            }
+            else 
+                cb(null, "Successfully added the task");
         });
     });
 
@@ -119,30 +133,31 @@ const addTask = (task, cb) =>{
 
 const updateTask = (taskId, editedTask, cb) =>{
 
-    // || !editedTask.date
     if(!editedTask.title || !editedTask.description || !editedTask.date) {
-        console.log("some fields missing");
+
+        if(err) {console.log("Error-dbCalls-updateTask: missing fields in updating task:", taskId, " error:" , err); 
+            cb("*Error: can not update the task. some fields are missing.");
+
+        }
     }
     if(!editedTask.finished) {
         editedTask.finished = false;
     }
-   /// let datePieces = editedTask.date.split("-");
-    // supported date formats in Javascript: YYYY-MM-DD or MM/DD/YYYY or 25 Mar 2015 or Mar 25 2015
-    
-    ///let newDate = datePieces[2]  + "-" + datePieces[1] + "-" + datePieces[0];
-    ///editedTask.date = Number(new Date(newDate));
-
-
-    //editedTask.date = Number(new Date());
-    console.log(editedTask);
 
     mongoClient.connect(url, (err, client) => {
+        
+        if(err) {console.error("Error-dbCalls-updateTask: problem connecting to DB while updating task:", taskId, " error:", err); 
+            cb("*Error: an error occured while updating the task.");
+
+        }
+
         let db = client.db(dbName);
         db.collection(collection).updateOne({"_id": new ObjectID(taskId)}, {$set: editedTask}, (err, result) => {
-            if(err){
-                console.log("Error in update task ",taskId); cb(err);
+            if(err) {console.error("Error-dbCalls-updateTask: problem updating task:", taskId, " error:", err); 
+                cb("*Error: an error occured while updating the task.");
+
             }else{
-                cb(null, "Successfully update the task")
+                    cb(null, "Successfully updated the task")
             }
         });
     });
